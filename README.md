e1ebae7d14d959d6092917099146c936823b19b0
Mikael Brockman <mikael@brockman.se>
2015-05-07 22:15:37 +0200

    Prototype

---
+++ README.md
@@ -0,0 +1,19 @@
+# bed, a simple tool for writing
+
+Writing can be fun. It can also be tedious. Sometimes it feels like
+everything is slipping away, or like the text you're writing is a big
+blob of amorphous stupidity. There are some tools that try to make
+writing more fun, simple, direct. This is another one. It makes you
+edit one line at a time. Or one paragraph at a time.
+
+It's inspired by chat. I noticed that when I'm chatting, there's a
+rhythm and liveliness that I miss when I face a text document. As far
+as I can tell, it's because chat has natural "units" or "chunks,"
+namely lines. You write a line and hit `Enter`. Then you write another
+one.
+
+Let's say `bed` stands for `better ed`. Of course, `ed` is the
+standard text editor on Unix systems. It's simple to the point of
+absurdity. Since it was invented decades ago, it forces you to write
+and edit line-by-line. Ever since its successor, `vi`, we've been
+using "visual" editors. But `ed` has its charms.
---
+++ index.js
@@ -0,0 +1,73 @@
+onload = function () {
+  React.render(tag(Root), document.body)
+}
+
+Root = React.createClass({
+  getInitialState: function () {
+    return { input: '', lines: [] }
+  },
+
+  change: function (e) {
+    this.setState({ input: e.target.value })
+  },
+
+  enter: function (e) {
+    e.preventDefault()
+    this.setState({
+      lines: this.state.lines.concat([this.state.input]),
+      input: ''
+    })
+  },
+
+  render: function () {
+    return tag('.bed', {}, [
+      tag('.lines', {}, [
+        this.state.lines.map(function (x, i) {
+          return tag('p', { key: i }, [x])
+        })
+      ]),
+      tag('form', { onSubmit: this.enter }, [
+        tag('input', {
+          autoFocus: true,
+          onChange: this.change,
+          value: this.state.input
+        })
+      ])
+    ])
+  }
+})
+
+update = React.addons.update
+
+// Create and return a new ReactElement of the given type.  The type
+// argument can be either an HTML tag name or a component class.
+function tag(type, props, children) {
+  if (type === void 0) {
+    throw new Error('Undefined first argument to tag()')
+  } else if (props instanceof Array) {
+    throw new Error('Second argument to tag() must be an object')
+  } else if (children && !(children instanceof Array)) {
+    throw new Error('Third argument to tag() must be an array')
+  } else if (typeof type != 'string') {
+    return React.createElement.apply(
+      React, [type, props].concat(children || [])
+    )
+  } else if (
+    // XXX: phew, what are we doing here...
+    /^([a-z0-9]+)?(?:#([a-z0-9-]+))?((?:\.[a-z0-9-]+)*)$/.test(type)
+  ) {
+    var tagName = RegExp.$1 || 'div'
+    var id = RegExp.$2 || void 0
+    var classNames = RegExp.$3 ? RegExp.$3.slice(1).split('.') : []
+    return React.createElement.apply(React, [tagName, update(props || {}, {
+      id: { $apply: function(x) { return x || id } },
+      className: {
+        $apply: function(x) {
+          return (x ? x.split(' ') : []).concat(classNames).join(' ')
+        }
+      }
+    })].concat(children || []))
+  } else {
+    throw new Error('Bad first argument to tag(): ' + JSON.stringify(type))
+  }
+}
